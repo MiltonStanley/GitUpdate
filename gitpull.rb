635046f475091403aa877cd1179abeafde5f0359
milton.stanley@gmail.com <milton.stanley@gmail.com>
2012-11-08 16:26:54 -0600

    Renamed gitget.rb to getpull.rb

--- gitpull.rb
+++ gitpull.rb
@@ -1,17 +1,14 @@
 require './config.rb'
 
-
-=begin
 def pull
   *repos = `cd .. && ls`.split "\n"
   puts "Found #{repos.join(', ')}"
   repos.each { |repo| system "cd .. && cd #{repo} && git pull origin master" }
 end
-=end
+
 
 mode = true if ARGV[0] == '-t' || ARGV[0] == '-true' || ARGV[0] == 't' || ARGV[0] == 'true'
 mode = false unless mode
-
 
 
 
@@ -19,12 +16,7 @@ print "Your password: "
 PASSWORD = gets.chomp
 puts PASSWORD
 
-*repos = `cd .. && ls`.split "\n"
-  puts "Found #{repos.join(', ')}"
-  repos.each { |repo| system "cd .. && cd #{repo} && git pull origin master" }
-
-#pull unless mode
 
 
 
-
+pull() unless mode
