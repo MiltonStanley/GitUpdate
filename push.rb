487e3536b283b0314dc7322df7a0a630e8ed6394
MiltonStanley <milton.stanley@gmail.com>
2012-11-09 23:50:54 -0600

    Added push

--- push.rb
+++ push.rb
@@ -1,11 +1,11 @@
-def pull
+def push
   *repos = `cd .. && ls`.split "\n"
   puts "Found #{repos.join(', ')}"
-  repos.each { |repo| system "cd .. && cd #{repo} && git pull origin master" }
+  repos.each { |repo| system "cd .. && cd #{repo} && git push origin master" }
 end
 
 
 mode = true if ARGV[0] == '-t' || ARGV[0] == '-true' || ARGV[0] == 't' || ARGV[0] == 'true'
 mode = false unless mode
 
-pull() unless mode
+push() unless mode
