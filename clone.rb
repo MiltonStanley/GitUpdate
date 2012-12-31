64af55395b7ace890a8e0fc89b091fff9e9b8697
MiltonStanley <milton.stanley@gmail.com>
2012-12-31 14:30:52 -0600

    Updated clone with new directory crawling

--- clone.rb
+++ clone.rb
@@ -2,5 +2,5 @@ repos = %w[Quandrax Quandrax_1.1.0 CPP_In_21_Days GitUpdate OrderServer Quandrax
 
 repos.each do |repo|
 	system "cd .. && git clone http://www.github.com/MiltonStanley/#{repo}.git"
-	system "cd #{repo} && git remote rm origin && git remote add origin https://github.com/MiltonStanley/#{repo}.git"
+	system "cd .. && cd #{repo} && git remote rm origin && git remote add origin https://github.com/MiltonStanley/#{repo}.git"
 end
