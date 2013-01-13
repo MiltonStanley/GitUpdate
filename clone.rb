b5acd48c2e126801ed4e6b756363fca482887637
MiltonStanley <milton.stanley@gmail.com>
2013-01-12 21:12:07 -0600

    Removed deprecated repos

--- clone.rb
+++ clone.rb
@@ -1,4 +1,5 @@
-repos = %w[Quandrax Quandrax_1.1.0 CPP_In_21_Days GitUpdate OrderServer Quandrax_Cpp tester Mythrealm RGB_Strip RecipeBook]
+# deprecated: Quandrax_1.1.0 Quandrax_Cpp OrderServer tester
+repos = %w[Quandrax CPP_In_21_Days GitUpdate Synthduino tester Mythrealm RGB_Strip RecipeBook]
 
 repos.each do |repo|
 	system "cd .. && git clone http://www.github.com/MiltonStanley/#{repo}.git"
