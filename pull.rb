a2a6721e09b0d54f3111e8bdc37be5b6e8fc7284
MiltonStanley <milton.stanley@gmail.com>
2013-02-14 11:28:21 -0600

    Program on hold - dangerous bugs found

--- pull.rb
+++ pull.rb
@@ -1,9 +1,13 @@
+puts "This program is dangerous. If a branch is checked out,"
+puts "it will overwrite that branch with master on the local machine."
+puts "Exiting now"
+Kernel.exit
+
 def pull
   *repos = `cd .. && ls`.split "\n"
   puts "Found #{repos.join(', ')}"
   repos.each { |repo| system "cd .. && cd #{repo} && git pull origin master" }
 end
-
 
 mode = true if ARGV[0] == '-t' || ARGV[0] == '-true' || ARGV[0] == 't' || ARGV[0] == 'true'
 mode = false unless mode
--- push.rb
+++ push.rb
@@ -1,3 +1,8 @@
+puts "This program is dangerous. If a branch is checked out,"
+puts "it will overwrite master on the remote repo."
+puts "Exiting now"
+Kernel.exit
+
 def push
   *repos = `cd .. && ls`.split "\n"
   puts "Found #{repos.join(', ')}"
